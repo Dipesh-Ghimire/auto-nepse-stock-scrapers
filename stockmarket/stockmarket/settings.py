4c0a0a2fe0460c80394e5e5241bb857aa82d8e63
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2025-06-18 11:44:32 +0545

    adding dotenv

--- stockmarket/stockmarket/settings.py
+++ stockmarket/stockmarket/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
